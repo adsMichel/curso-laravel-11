d9c86bf585526422a347de58b4f79649c508ce79
Michel Naoki <lojachimbinha@gmail.com>
2024-06-14 13:56:54 -0300

    add layout

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -3,15 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title') - Especializa TI</title>
-
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <header>header default2</header>
+    <header>
+        <h4>Header Default</h4>
+    </header>
     @yield('content')
-    <footer>footer2</footer>
+    <footer>
+        <h4>Footer Default</h4>
+    </footer>
 </body>
 </html>
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,37 +1,28 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+@extends('admin.layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Listar Usuários</title>
-</head>
+@section('title', 'Listagem dos Usuários')
 
-<body>
-    <h1>Usuários</h1>
-    <a href="{{ route('users.create') }}">Novo</a>
+@section('content')
+<h1>Usuários</h1>
+<a href="{{ route('users.create') }}">Novo</a>
 
-    <table>
-        <tr>
-            <th>Nome</th>
-            <th>E-mail</th>
-            <th>Ações</th>
-        </tr>
-        @forelse ($users as $user)
-            <tr>
-                <td>{{ $user->name }}</td>
-                <td>{{ $user->email }}</td>
-                <td>
-                    <a href="">Editar</a>
-                </td>
-            </tr>
-        @empty
-            <p>Não há usuários</p>
-        @endforelse
-    </table>
+<table>
+    <tr>
+        <th>Nome</th>
+        <th>E-mail</th>
+        <th>Ações</th>
+    </tr>
+    @forelse ($users as $user)
+    <tr>
+        <td>{{ $user->name }}</td>
+        <td>{{ $user->email }}</td>
+        <td><a href="">Editar</a></td>
+    </tr>
+    @empty
+    <p>Não há usuários</p>
+    @endforelse
+</table>
 
-    {{ $users->links() }}
-</body>
+{{ $users->links() }}
 
-</html>
+@endsection
